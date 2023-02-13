5fc123b65ed90fa05776efc3be4e6881c4a1ac6f
ljs <qazplm807733@gmail.com>
2023-02-13 15:26:35 +0900

    Day 2. Complete simple shell

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rust_Simple_shell
+
+## 간단하게 만들 수 있길래 실습 해봄.
+
+## link
+
+https://www.joshmcguigan.com/blog/build-your-own-shell-rust/
+
+## Day 1. Make Start Point.
+
+1. Make start point for simple shell command
+
+## Day 2. Complete simple Shell
+
+1. Multiple Commands
+
+2. Handling Args
+
+3. Built-ins (cd ~)
+
+4. Error Handling
+
+5. Pipes
--- simple_shell/src/main.rs
+++ simple_shell/src/main.rs
@@ -1,10 +1,70 @@
-use std::{io::stdin, process::Command};
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process::{Child, Command, Stdio},
+};
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
 
-    let com = input.trim();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    Command::new(com).spawn().unwrap();
+        let mut commands = input.split(" | ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next() {
+            let mut parts = command.split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                }
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
+                }
+            }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            // block until the final command has finished
+            final_command.wait().unwrap();
+        }
+    }
 }
